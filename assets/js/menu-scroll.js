40c5ccb9c6c54d6f1abf6985e382e898b221a190
Creativm <creativm@users.noreply.github.com>
2019-12-06 21:34:59 +0930

    .

--- assets/js/menu-scroll.js
+++ assets/js/menu-scroll.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	$(window).scroll(function() {
 		if ($(document).scrollTop() > 150) {
 			$("#menu").css('background-color', '#0087c9');
-			$("#menu").css('transition', '0.5s');
+			$("#menu").css('transition', '0.2s');
 		} else {
 			$('#menu').css('background-color', 'transparent');
 		}
